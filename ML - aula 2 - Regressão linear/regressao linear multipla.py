7f8f1a7a7a56ade3b3017f1f6c3136535c808439
Thiago MM <thiagommergener@gmail.com>
2024-04-15 21:21:53 -0300

    Enhance data pre-processing and implement linear regression model
    Refactored the script for handling missing values and categorizing variables in the process of data pre-processing. Implemented Linear Regression and DecisionTreeRegressor models to predict housing prices and evaluated the models using Mean Squared Error, Mean Absolute Error, R squared, and Mean Absolute Percentage Error. A new file was added in the 'OUTROS/Regressao linear multipla' directory, implementing multiple variables linear regression to predict salaries.

--- ML - aula 2 - Regressão linear/regressao linear multipla.py
+++ ML - aula 2 - Regressão linear/regressao linear multipla.py
@@ -39,7 +39,7 @@ print(dataset.describe())
 # %matplotlib inline
 import matplotlib.pyplot as plt
 dataset.hist(bins=50, figsize=(20, 15))
-# plt.show()
+plt.show()
 
 ### SEPARANDO AS BASES EM TREINO E TESTE
 from sklearn.model_selection import train_test_split
@@ -55,7 +55,7 @@ print('\n', len(df_train), 'treinamento +', len(df_test), 'teste\n')
 # representativos com todos os valores de renda anual.Como a média de renda anual é um atributo numérico,
 # que tal criar uma categoria de renda ?
 dataset["median_income"].hist()
-# plt.show()
+plt.show()
 # Divida por 1,5 para limitar o número de categorias de renda
 # dividindo o valor da coluna "median_income" de cada entrada pelo valor 1,5 e,
 # em seguida, arredondando o resultado para cima usando a função
@@ -100,11 +100,11 @@ for set_ in (strat_train_set, strat_test_set):
 # ANALISANDO DADOS GEOGRÁFICOS
 housing = strat_train_set.copy()  # Realiza uma cópia da base
 housing.plot(kind="scatter", x="longitude", y="latitude")
-# plt.show()
+plt.show()
 # Com alpha 0.1 setado, é possível analisar a concentração de dados
 # em determinados pontos do gráfico (densos e não densos)
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
-# plt.show()
+plt.show()
 # A concentração das localidades no gráfico terá um formato semelhante ao do estado da Califórnia,
 # que é a localidade dos dados
 
@@ -116,7 +116,7 @@ housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
              c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
              sharex=False)
 plt.legend()
-# plt.show()
+plt.show()
 
 # BUSCANDO CORRELAÇÕES
 corr_matrix = housing.corr(numeric_only=True)
@@ -144,3 +144,236 @@ plt.show()
 # não pode afetar nosso algoritmo?
 
 ### PREPARANDO OS DADOS PARA COLOCAR NO ALGORITMO
+housing = strat_train_set.drop("median_house_value", axis=1)  # apagando a target para a base de treino (nosso x)
+housing_labels = strat_train_set["median_house_value"].copy()  # armazenando a target (nosso y)
+# listando as colunas nulas
+sample_incomplete_rows = housing[housing.isnull().any(axis=1)].head()
+print(sample_incomplete_rows)
+print(housing.isnull().sum())
+# OK, como vamos tratar esses valores nulos?
+# Opção 1
+# Substituindo os valores nulos pela mediana
+median = housing["total_bedrooms"].median()
+sample_incomplete_rows["total_bedrooms"].fillna(median, inplace=True)
+print(sample_incomplete_rows)
+
+# UTILIZANDO AS CLASSES DO SKLEARN
+# Você também pode optar por utilizar classes acessíveis do Sklearn!
+# Você pode criar pipelines de pré-processamento e modelagem com facilidade usando as classes do Scikit-Learn.
+# Isso permite criar fluxos de trabalho mais organizados e repetíveis.
+# Vamos utilizar o Imputer para substituir os valores faltantes pela média.
+# Opção 2
+try:
+    from sklearn.impute import SimpleImputer # Scikit-Learn 0.20+
+except ImportError:
+    from sklearn.preprocessing import Imputer as SimpleImputer
+imputer = SimpleImputer(strategy="median")
+
+# Remova o atributo de texto porque a mediana só pode ser calculada em atributos numéricos:
+housing_num = housing.drop('ocean_proximity', axis=1)
+imputer.fit(housing_num)  # calculando a mediana de cada atributo e armazenando o resultado na variável statistics_
+
+print('\n')
+print(imputer.statistics_)
+
+# Verifique se isso é o mesmo que calcular manualmente a mediana de cada atributo:
+print(housing_num.median().values)
+
+# Aplicando o Imputer "treinado" na base para substituir valores faltantes perdidos pela mediana:
+X = imputer.transform(housing_num) # o resultado é um array.
+print(X)
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing.index)
+print(housing_tr)
+
+# verificando os resultados
+print(housing_tr.loc[sample_incomplete_rows.index.values])
+
+print(imputer.strategy)
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing_num.index)
+print(housing_tr.head())
+
+
+# PRÉ-PROCESSANDO AS CATEGORIAS
+# Agora vamos pré-processar o recurso de entrada categórica, ocean_proximity:
+housing_cat = housing[['ocean_proximity']]
+print(housing_cat.head(10))
+
+# O OrdinalEncoder é uma classe da biblioteca scikit-learn, usada para transformar variáveis categóricas ordinais
+# em valores numéricos. Variáveis ordinais são aquelas que têm uma ordem ou hierarquia específica,
+# mas as distâncias entre os valores não são necessariamente significativas.
+try:
+    from sklearn.preprocessing import OrdinalEncoder
+except ImportError:
+    from future_encoders import OrdinalEncoder # Scikit-Learn < 0.20
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+print(housing_cat_encoded[:10])
+
+print(ordinal_encoder.categories_)
+
+# O OneHotEncoder é outra classe da biblioteca scikit-learn,
+# usada para transformar variáveis categóricas em representações numéricas binárias.
+# Ele é particularmente útil quando se lida com variáveis categóricas nominais,
+# ou seja, aquelas que não têm uma ordem específica.
+try:
+    from sklearn.preprocessing import OrdinalEncoder # apenas para gerar um ImportError se Scikit-Learn < 0.20
+    from sklearn.preprocessing import OneHotEncoder
+except ImportError:
+    from future_encoders import OneHotEncoder # Scikit-Learn < 0.20
+
+cat_encoder = OneHotEncoder(sparse_output=False)
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+print(housing_cat_1hot)
+print(cat_encoder.categories_)
+
+# CRIANDO A PIPELINA DE PRÉ-PROCESSAMENTO
+# Agora vamos construir um pipeline para pré-processar os atributos numéricos:
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")), #substituindo valores nulos pela mediana
+    ('std_scaler', StandardScaler()), # padronizando as escalas dos dados
+])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+print(housing_num_tr)
+
+# Agora, vamos tratar os valores categóricos:
+# O ColumnTransformer é uma classe da biblioteca scikit-learn em Python
+# que permite aplicar transformações específicas a diferentes colunas de um conjunto de dados
+# (dados numéricos, categóricos, etc.) e deseja aplicar diferentes pré-processamentos
+# ou transformações a cada tipo de coluna.
+try:
+    from sklearn.compose import ColumnTransformer
+except ImportError:
+    from future_encoders import ColumnTransformer # Scikit-Learn < 0.20
+
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),  # tratando as variáveis numéricas (chamando a pipeline de cima)
+    ("cat", OneHotEncoder(), cat_attribs),  # tratando as variáveis categóricas
+])
+housing_prepared = full_pipeline.fit_transform(housing)
+
+print(housing_prepared)
+
+print('\n')
+print(housing_prepared.shape)
+print(type(housing_prepared))
+
+
+# Perceba que o resultado é uma matriz multidimensional. Precisamos transformá-la em dataframe.
+column_names = [
+    'longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms',
+    'population', 'households', 'median_income', '<1H OCEAN', 'INLAND', 'ISLAND', 'NEAR BAY', 'NEAR OCEAN']
+# Transformar o array em DataFrame
+housing_df = pd.DataFrame(data=housing_prepared, columns=column_names)
+# Exibir o DataFrame resultante
+print(housing_df.shape)
+
+print(housing_df.head())
+print(housing_df.isnull().sum())
+
+
+
+
+
+
+# ESCOLHENDO O MELHOR MODELO DE REGRESSÃO
+# Vamos começar com a velha e boa regressão linear!
+# - Equação do 1° grau.
+# - A Regressão Linear busca entender o padrão de um valor dependendo de outro ou outros, e assim encontrar uma função que expressa esse padrão.
+# - Foco: buscar o melhor valor que os coeficientes possam atingir, de maneira que a diferença entre o valor predito pela função e o real, sejam os menores.
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)  # Treinamento
+
+# vamos tentar o pipeline de pré-processamento completo em algumas instâncias de treinamento
+some_data = housing.iloc[:5]
+some_labels = housing_labels.iloc[:5]
+some_data_prepared = full_pipeline.transform(some_data)
+predictions = lin_reg.predict(housing_prepared)
+print("Predictions:", lin_reg.predict(some_data_prepared))
+
+# Compare com os valores reais:
+print("Labels:", list(some_labels))
+
+
+# AVALIANDO O MODELO
+# O MSE mede a média dos quadrados das diferenças entre os valores previstos pelo modelo
+# e os valores reais observados no conjunto de dados.
+# Quanto menor o valor do MSE, melhor o ajuste do modelo aos dados.
+from sklearn.metrics import mean_squared_error
+# erro médio quadrático eleva ao quadrado a média do erro médio absoluto.
+# Estou avaliando se os erros não são tão grandes, esses erros são penalizados.
+# penaliza muito mais valores distantes da média.
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)  # raiz quadrada aqui
+print(lin_rmse)
+
+from sklearn.metrics import mean_absolute_error
+lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+print(lin_mae)
+
+# Um erro de margem de 69050 dólares não é muito aceitável no nosso modelo
+# sendo que os valores de median_housing_values variam entre 120 mil dólares e 265 mil dólares
+# Podemos definir aqui que esse modelo está com overfiting.
+# Vamos tentar um modelo mais poderoso?
+from sklearn.metrics import r2_score
+r2 = r2_score(housing_labels, housing_predictions)
+print('r²', r2)
+
+
+# Função para calcular o MAPE (Mean Absolute Percentage Error)
+def calculate_mape(labels, predictions):
+    errors = np.abs(labels - predictions)
+    relative_errors = errors / np.abs(labels)
+    mape = np.mean(relative_errors) * 100
+    return mape
+
+
+# Calcular o MAPE
+mape_result = calculate_mape(housing_labels, housing_predictions)
+
+# Imprimir o resultado
+print(f"O MAPE é: {mape_result:.2f}%")
+
+# QUE TAL TENTAR OUTROS MODELOS?
+from sklearn.tree import DecisionTreeRegressor
+# Criando o modelo de DecisionTreeRegressor
+model_dtr = DecisionTreeRegressor(max_depth=10)  # Número de ramificações da árvore, atenção para não gerar overfitting
+model_dtr.fit(housing_prepared, housing_labels)
+
+# vamos tentar o pipeline de pré-processamento completo em algumas instâncias de treinamento
+some_data = housing.iloc[:5]
+some_labels = housing_labels.iloc[:5]
+some_data_prepared = full_pipeline.transform(some_data)
+predictions = model_dtr.predict(some_data_prepared)
+print("Predictions:", model_dtr.predict(some_data_prepared))
+
+print("Labels:", list(some_labels))
+
+# mean_squared_error
+housing_predictions = model_dtr.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+print(lin_rmse)
+
+# mean_absolute_error
+lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+print(lin_mae)
+
+r2 = r2_score(housing_labels, housing_predictions)
+print('r²', r2)
+
+# Calcular o MAPE
+mape_result = calculate_mape(housing_labels, housing_predictions)
+
+# Imprimir o resultado
+print(f"O MAPE é: {mape_result:.2f}%")
---
+++ OUTROS/Regressao linear multipla/rlm.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 17:09:56 2021
+
+@author: rafaeldontalgoncalez
+"""
+
+######################################
+# Importando as libraries
+######################################
+
+import pandas as pd
+import sklearn.model_selection as ms
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+import numpy as np
+
+######################################
+# Importa o dataset
+######################################
+
+
+dataset = pd.read_csv("/Users/rafaeldontalgoncalez/Downloads/2020_Data_Professional_Salary_MultipleFeatures.csv")
+dataset = dataset.dropna()
+X = dataset.iloc[:, :-2]
+y = dataset.iloc[:, -1].values
+
+######################################
+# Codificando variaveis Dummy
+######################################
+X_dummies = pd.get_dummies(X)
+
+######################################
+# Separar dados em Treino e Teste
+######################################
+
+X_train, X_test, y_train, y_test = ms.train_test_split(X_dummies, y, test_size=1 / 5, random_state=0)
+
+######################################
+# Treinando o modelo
+######################################
+
+regressor = lm.LinearRegression()
+regressor.fit(X_train, y_train)
+
+######################################
+# Previsao
+######################################
+
+y_pred = regressor.predict(X_test)
+
+np.set_printoptions(precision=2)
+result = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
+
+
+def undummify(df, prefix_sep="_"):
+    cols2collapse = {
+        item.split(prefix_sep)[0]: (prefix_sep in item) for item in df.columns
+    }
+    series_list = []
+    for col, needs_to_collapse in cols2collapse.items():
+        if needs_to_collapse:
+            undummified = (
+                df.filter(like=col)
+                .idxmax(axis=1)
+                .apply(lambda x: x.split(prefix_sep, maxsplit=1)[1])
+                .rename(col)
+            )
+            series_list.append(undummified)
+        else:
+            series_list.append(df[col])
+    undummified_df = pd.concat(series_list, axis=1)
+    return undummified_df
+
+
+X_reverse = undummify(X_test)
+
+X_reverse = X_reverse.reset_index(drop=True)
+
+y_compare = pd.DataFrame(result)
+y_compare = y_compare.rename(index=str, columns={0: 'y_pred', 1: 'y_test'})
+y_compare = y_compare.reset_index(drop=True)
+
+resultado_final = pd.concat([y_compare, X_reverse], axis=1)
+
+######################################
+# Valor Especifico
+######################################
+
+print(regressor.predict([[10]]))
