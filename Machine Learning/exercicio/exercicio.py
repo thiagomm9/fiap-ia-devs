b5cd5169ac870e59c1f8471e4db7ed643ab75d2b
Thiago MM <thiagommergener@gmail.com>
2024-04-23 20:45:40 -0300

    Ajustes na estrutura de pastas e inicia exercicio de ML

---
+++ Machine Learning/exercicio/exercicio.py
@@ -0,0 +1,28 @@
+"""Chegou a hora de compilar todos os aprendizados obtidos nas aulas de Machine Learning, em um único lugar,
+para fazer a entrega do Desafio da disciplina. Utilizando a base de dados “insurance.csv”, você tem o desafio de
+criar um modelo preditivo de regressão para prever o valor dos custos médicos individuais cobrados pelo seguro de saúde.
+
+Sobre a base de dados Essa base de dados contém 1.338 linhas com informações sobre a idade da pessoa, gênero,
+índice de massa corporal (IMC), número de filhos, flag de verificação se a pessoa é fumante, região residencial do
+benefício e o valor do custo médico.
+
+Dicionário dos dados
+Idade: idade do beneficiário principal.
+
+Gênero: gênero do contratante de seguros.
+
+IMC: índice de massa corporal, fornecendo uma compreensão do corpo, pesos relativamente altos ou baixos em relação à
+altura.
+
+Filhos: número de filhos cobertos por seguro saúde / Número de dependentes.
+
+Fumante: se a pessoa fuma (sim ou não).
+
+Região: a área residencial do beneficiário nos EUA, nordeste, sudeste, sudoeste ou noroeste.
+
+Encargos: custos médicos individuais cobrados pelo seguro de saúde.
+
+Objetivo
+Criar um modelo preditivo e comprovar sua eficácia com métricas estatísticas.
+"""
+
