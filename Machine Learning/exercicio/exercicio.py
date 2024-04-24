024784dabfbcbb77037355e03faf8e4583a9bee3
Thiago MM <thiagommergener@gmail.com>
2024-04-24 20:52:46 -0300

    Implement machine learning predictions in exercicio.py
    The machine learning implementation includes loading the dataset, initial exploratory data analysis, preprocessing such as label encoding, and model training using linear regression. Further, evaluation metrics such as Mean Squared Error (MSE), Mean Absolute Error (MAE), r2 score, and Mean Absolute Percentage Error (MAPE) have been calculated and displayed.

--- Machine Learning/exercicio/exercicio.py
+++ Machine Learning/exercicio/exercicio.py
@@ -25,4 +25,90 @@ Encargos: custos médicos individuais cobrados pelo seguro de saúde.
 Objetivo
 Criar um modelo preditivo e comprovar sua eficácia com métricas estatísticas.
 """
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
+# Read insurance.csv file to data frame
+df = pd.read_csv("insurance.csv")
+# print(df.shape)
+# print(df.info())
+
+# Check for missing values
+# print(df.isnull().sum())
+
+# Get the different values the column region
+# print(df['region'].unique())
+
+# Get the different values the column sex
+# print(df['sex'].unique())
+
+# Get the different values the column smoker
+# print(df['smoker'].unique())
+
+# Visualiza histograma
+df.hist(bins=50, figsize=(20, 15))
+plt.show()
+
+# label encode necessary columns
+le = LabelEncoder()
+df['region'] = le.fit_transform(df['region'])
+df['sex'] = le.fit_transform(df['sex'])
+df['smoker'] = le.fit_transform(df['smoker'])
+
+# Convert categorical variables to numerical using one-hot encoding
+# df = pd.get_dummies(df)
+
+# Calculate the correlation matrix
+correlation_matrix = df.corr()
+
+# Plot correlation matrix
+# plt.figure(figsize=(10, 8))
+# sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.6)
+# plt.show()
+
+# Split db to X and y
+X = df.drop(columns=['charges'])  # Features
+y = df['charges']  # Target
+
+# Split db to train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model with linear regression
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Calculate MSE
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+mse = np.sqrt(mse)  # raiz quadrada aqui
+print(mse)
+
+# Calculate MAE
+mae = mean_absolute_error(y_test, y_pred)
+print(mae)
+
+# Calculate r2 score
+r2 = r2_score(y_test, y_pred)
+print('r²', r2)
+
+
+# Função para calcular o MAPE (Mean Absolute Percentage Error)
+def calculate_mape(labels, predictions):
+    errors = np.abs(labels - predictions)
+    relative_errors = errors / np.abs(labels)
+    mape = np.mean(relative_errors) * 100
+    return mape
+
+
+# Calcular o MAPE
+mape_result = calculate_mape(y_test, y_pred)
+
+# Imprimir o resultado
+print(f"O MAPE é: {mape_result:.2f}%")
